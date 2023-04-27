46361317f7fcf7e58cc79fe9626645eb657ab72b
Takehiro Izumi <33809675+sytk@users.noreply.github.com>
2023-04-27 16:43:52 +0200

    marge

--- jeti.py
+++
@@ -1,8 +0,0 @@
-from luxpy.toolboxes import spectro as sp
-import luxpy as lx  # package for color science calculations
-from matplotlib import pyplot as plt
-
-a = sp.init('jeti')
-spd = sp.jeti.get_spd()
-lx.SPD(spd).plot()
-plt.show()
